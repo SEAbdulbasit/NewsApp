5e6f9f65b1d51e1d353422df07bc80671a217885
Abdul Basit <abdulbasit5361234@gmail.com>
2023-08-17 21:45:37 +0500

    Add initial screen

--- app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
@@ -1,46 +1,33 @@
-package com.example.newsapp
+package com.example.newsapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.newsapp.presentation.headlineslist.NewsHeadlineViewModel
+import com.example.newsapp.presentation.headlineslist.NewsHeadlinesRoute
 import com.example.newsapp.ui.theme.NewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NewsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                val startRoute = "list"
+                NavHost(navController, startDestination = startRoute) {
+                    composable("list") { backStackEntry ->
+                        val viewModel = hiltViewModel<NewsHeadlineViewModel>()
+                        NewsHeadlinesRoute(viewModel)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsAppTheme {
-        Greeting("Android")
     }
 }
