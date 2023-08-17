14739498ac8cff04d1c9ad04d049384174fe0898
Abdul Basit <abdulbasit5361234@gmail.com>
2023-08-17 20:16:14 +0500

    Add view model and test

--- app/src/main/java/com/example/newsapp/presentation/headlineslist/NewsHeadlineViewModel.kt
+++ app/src/main/java/com/example/newsapp/presentation/headlineslist/NewsHeadlineViewModel.kt
@@ -1,8 +1,9 @@
-package com.example.newsapp.presentation
+package com.example.newsapp.presentation.headlineslist
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.newsapp.domain.NewsRepository
+import com.example.newsapp.presentation.NewsScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -26,7 +27,7 @@ class NewsHeadlineViewModel @Inject constructor(private val newsRepository: News
         onAction(NewsHeadlinesActions.Refresh)
     }
 
-    private fun onAction(actions: NewsHeadlinesActions) {
+    fun onAction(actions: NewsHeadlinesActions) {
         when (actions) {
             NewsHeadlinesActions.Refresh -> {
                 fetchHeadlines()
@@ -36,6 +37,7 @@ class NewsHeadlineViewModel @Inject constructor(private val newsRepository: News
 
     private fun fetchHeadlines() {
         viewModelScope.launch {
+            _state.value = _state.value.copy(isLoading = true)
             newsRepository.getHeadlines().fold(
                 onSuccess = { articlesList ->
                     _state.update {
---
+++ app/src/test/java/com/example/newsapp/presentation/headlineslist/NewsHeadlineViewModelTest.kt
@@ -0,0 +1,152 @@
+package com.example.newsapp.presentation.headlineslist
+
+import app.cash.turbine.test
+import com.example.newsapp.domain.NewsRepository
+import com.example.newsapp.domain.model.ArticleDomainModel
+import com.example.newsapp.domain.model.SourceDomainModel
+import com.example.newsapp.presentation.NewsScreenState
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+
+/**
+ * Created by abdulbasit on 17/08/2023.
+ */
+@OptIn(ExperimentalCoroutinesApi::class)
+class NewsHeadlineViewModelTest {
+
+    private val newsRepository: NewsRepository = mockk()
+    private lateinit var viewModel: NewsHeadlineViewModel
+
+    private val exception = Exception("Something went wrong")
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(StandardTestDispatcher())
+    }
+
+    @Test
+    fun viewModelCallsGetHeadlines() = runTest {
+        coEvery { newsRepository.getHeadlines() } returns Result.success(emptyList())
+
+        viewModel = NewsHeadlineViewModel(newsRepository)
+
+        coVerify { newsRepository.getHeadlines() }
+    }
+
+    @Test
+    fun stateShouldBeLoadingAndThenSuccessWhenHeadlinesReturnsArticlesList() = runTest {
+        coEvery { newsRepository.getHeadlines() } returns Result.success(emptyList())
+
+        viewModel = NewsHeadlineViewModel(newsRepository)
+        viewModel.state.test {
+            assertEquals(NewsScreenState(), awaitItem())
+            assertEquals(NewsScreenState(isLoading = false, articles = emptyList()), awaitItem())
+        }
+    }
+
+    @Test
+    fun stateShouldBeLoadingAndThenFailureWhenHeadlinesReturnsFailure() = runTest {
+        coEvery { newsRepository.getHeadlines() } returns Result.failure(exception)
+
+        viewModel = NewsHeadlineViewModel(newsRepository)
+        viewModel.state.test {
+            assertEquals(NewsScreenState(), awaitItem())
+            assertEquals(
+                NewsScreenState(
+                    isLoading = false, articles = emptyList(), "Something went wrong"
+                ), awaitItem()
+            )
+        }
+    }
+
+    @Test
+    fun givenRefreshActionStateShouldBeLoadingAndTheSuccessWhenHeadlinesReturnsArticlesList() =
+        runTest {
+
+            coEvery { newsRepository.getHeadlines() } returns Result.success(emptyList())
+
+            viewModel = NewsHeadlineViewModel(newsRepository)
+
+            viewModel.state.test {
+                assertEquals(NewsScreenState(), awaitItem())
+                assertEquals(
+                    NewsScreenState(
+                        isLoading = false, articles = emptyList(), exceptionMessage = null
+                    ), awaitItem()
+                )
+
+                viewModel.onAction(NewsHeadlinesActions.Refresh)
+
+                assertEquals(
+                    NewsScreenState(isLoading = true, articles = emptyList(), null), awaitItem()
+                )
+                assertEquals(
+                    NewsScreenState(
+                        isLoading = false, articles = emptyList(), exceptionMessage = null
+                    ), awaitItem()
+                )
+
+            }
+        }
+
+    @Test
+    fun givenRefreshActionStateShouldBeLoadingAndTheFailureWhenHeadlinesReturnsFailure() {
+        runTest {
+            coEvery { newsRepository.getHeadlines() } returns Result.success(listOf(articleDomainModel)) andThen Result.failure(
+                exception
+            )
+
+            viewModel = NewsHeadlineViewModel(newsRepository)
+
+            viewModel.state.test {
+                assertEquals(NewsScreenState(), awaitItem())
+                assertEquals(
+                    NewsScreenState(
+                        isLoading = false, articles = listOf(articleDomainModel), exceptionMessage = null
+                    ), awaitItem()
+                )
+
+                viewModel.onAction(NewsHeadlinesActions.Refresh)
+
+                assertEquals(
+                    NewsScreenState(isLoading = true, articles = listOf(articleDomainModel), null), awaitItem()
+                )
+                assertEquals(
+                    NewsScreenState(
+                        isLoading = false, articles = listOf(articleDomainModel), exceptionMessage = "Something went wrong"
+                    ), awaitItem()
+                )
+
+            }
+        }
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    private val articleDomainModel = ArticleDomainModel(
+        author = "abc",
+        content = "This is content",
+        description = "This is description",
+        publishedAt = "12/12/2022",
+        source = SourceDomainModel("1", "Json"),
+        title = "title",
+        url = "url",
+        urlToImage = "urlToImage"
+    )
+
+}
